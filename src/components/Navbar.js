b8f46003a3713a2da5613713716da4efe6c7cbf2
Accelerator321 <shyamprime2610@gmail.com>
2023-04-12 22:41:14 +0530

    final 12 april

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,22 +7,22 @@ function Navbar() {
     return (<>
         
 
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <nav className="navbar navbar-dark navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/" style={{color:'white'}}>Notex</a>
+    <Link className="navbar-brand" href="/" style={{color:'white'}}>Notex</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon" />
     </button>
     <div className="collapse navbar-collapse" id="navbarNavDropdown">
       <ul className="navbar-nav">
-        <li className="nav-item my2 mx-2">
+        <li className="nav-item my2 mx-2" style={{color:'white'}}>
                     Select  Bucket:
                             <BucketSelector />
         </li>
         <li className="nav-item my-2 mx-2">
-        <Link className='btn btn-outline-primary' to='/newitem'>Add new item</Link>
+        <Link className='btn btn-outline-primary' to='/newitem' style={{color:'white'}}>Add new item</Link>
         </li>
-        <li className="nav-item my-2 mx-2">
+        <li className="nav-item my-2 mx-2" style={{color:'white'}}>
         <NewBucket/>
         </li>
         
